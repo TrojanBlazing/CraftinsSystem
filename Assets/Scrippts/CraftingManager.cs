04911e888cff5a77da1f2c3f5cbf94a4d7d2bf26
TrojanBlazing <145752343+TrojanBlazing@users.noreply.github.com>
2024-06-09 10:44:12 +0530

    all

--- Assets/Scrippts/CraftingManager.cs
+++ Assets/Scrippts/CraftingManager.cs
@@ -1,10 +1,8 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-
 
 public class CraftingManager : MonoBehaviour
 {
@@ -14,90 +12,106 @@ public class CraftingManager : MonoBehaviour
     public Slot[] craftingSlots;
 
     public List<Item> itemList;
-    public string[] Itemrecipes;
-    public Item[] Itemresults;
-    public Slot ItemResultsSlots;
+    public List<string> itemRecipes;
+    public List<Item> itemResults;
+    public Slot itemResultSlot;
 
-     private void Update()
+    private void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
-            if(cItem!= null)
+            if (cItem != null)
             {
                 customCursor.gameObject.SetActive(false);
                 Slot nearSlot = null;
-                float shortDistance=float.MaxValue;
+                float shortDistance = float.MaxValue;
 
-                foreach(Slot slot in craftingSlots)
+                foreach (Slot slot in craftingSlots)
                 {
-                    float dist=Vector2.Distance(Input.mousePosition, slot.transform.position);
+                    float dist = Vector2.Distance(Input.mousePosition, slot.transform.position);
 
-
-                    if(dist<shortDistance)
+                    if (dist < shortDistance)
                     {
                         shortDistance = dist;
                         nearSlot = slot;
                     }
                 }
-                nearSlot.gameObject.SetActive(true);    
-                nearSlot.GetComponent<Image>().sprite=cItem.GetComponent<Image>().sprite;
-                nearSlot.item = cItem;
 
-                itemList[nearSlot.index] = cItem;
-                cItem = null;
+                if (nearSlot != null)
+                {
+                    nearSlot.gameObject.SetActive(true);
+                    nearSlot.GetComponent<Image>().sprite = cItem.GetComponent<Image>().sprite;
+                    nearSlot.item = cItem;
+                    itemList[nearSlot.index] = cItem;
+                    cItem = null;
 
-                CheckRecipes();
+                    CheckRecipes();
+                }
             }
         }
     }
 
     private void CheckRecipes()
     {
-        ItemResultsSlots.gameObject.SetActive(false);
-        ItemResultsSlots.item = null;
-        string currentRecipe = "";
+        itemResultSlot.gameObject.SetActive(false);
+        itemResultSlot.item = null;
 
-        foreach(Item item in itemList)
+        List<string> currentRecipe = new List<string>();
+
+        foreach (Item item in itemList)
         {
-            if(item!=null)
+            if (item != null)
             {
-                currentRecipe += item.Name;
-            }
-            else
-            {
-                currentRecipe += "null";
+                currentRecipe.Add(item.ID); 
             }
         }
 
-        for(int i =0;i<Itemrecipes.Length;i++)
+        currentRecipe.Sort();
+
+        foreach (string recipe in itemRecipes)
         {
-            if (Itemrecipes[i]==currentRecipe)
+            List<string> sortedRecipe = new List<string>(recipe.Split(','));
+            sortedRecipe.Sort();
+
+            if (AreListsEqual(currentRecipe, sortedRecipe))
             {
-                ItemResultsSlots.gameObject.SetActive(true);
-                ItemResultsSlots.GetComponent<Image>().sprite = Itemresults[i].GetComponent<Image>().sprite;
-                ItemResultsSlots.item = Itemresults[i]; 
+                int index = itemRecipes.IndexOf(recipe);
+                if (index >= 0 && index < itemResults.Count)
+                {
+                    itemResultSlot.gameObject.SetActive(true);
+                    itemResultSlot.GetComponent<Image>().sprite = itemResults[index].GetComponent<Image>().sprite;
+                    itemResultSlot.item = itemResults[index];
+                }
+                break;
             }
         }
-
     }
 
-    public void OnclickSlot(Slot slot)
+    private bool AreListsEqual(List<string> list1, List<string> list2)
+    {
+        if (list1.Count != list2.Count) return false;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list1[i] != list2[i]) return false;
+        }
+        return true;
+    }
+
+    public void OnClickSlot(Slot slot)
     {
         slot.item = null;
         itemList[slot.index] = null;
-       slot.gameObject.SetActive(false);
+        slot.gameObject.SetActive(false);
         CheckRecipes();
-
-
     }
+
     public void OnMouseDownItem(Item item)
     {
-        if(cItem==null)
+        if (cItem == null)
         {
             cItem = item;
             customCursor.gameObject.SetActive(true);
             customCursor.sprite = cItem.GetComponent<Image>().sprite;
-
         }
     }
 }
